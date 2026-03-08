4233b25184a9f0e37184aecfb4733aa48633b675
ManishaKesarwani <manishakesarwani9354@gmail.com>
2026-03-08 13:09:07 +0530

    domain email updated

--- backend/SendEmail/SendEmail.js
+++ backend/SendEmail/SendEmail.js
@@ -1,33 +1,28 @@
-const Mailjet = require("node-mailjet");
+const {Resend} = require("resend");
 const fs=require("fs");
 const path=require("path");
 
 require("dotenv").config({path: path.resolve(__dirname, "../.env")});
 
-const mailjet = Mailjet.apiConnect(
-    process.env.MAILJET_API_KEY,
-    process.env.MAILJET_SECRET_KEY
-);
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 exports.send_Mail = async (reciever, emailSubject, emailBody) => {
     try{
         
 
-        const response = await mailjet.post("send", {version: "v3.1"}).request({
-            Messages:[{
-                From: {
-                    Email: "manishakesarwani9354@gmail.com",
-                    Name: "Visitor Management System"
-                },
-                To: [{
-                    Email: reciever
-                }],
-                Subject: emailSubject,
-                TextPart: "Visitor Management System Notification",
-                HTMLPart: emailBody
-            }]
-        })
-         console.log("Email response", response.body);
+        const response = await resend.emails.send({
+            from: "VMS-verified-email@visitormanagementsystem.shop",
+            to: reciever,
+            subject: emailSubject,
+            html: emailBody
+        });
+        if(response.error){
+            console.log("Email sent failed", response.error);
+        }
+        else{
+            console.log("Email sent", response.data);
+        }
+         
     }catch(err){
         console.log("Error", err.message);
         throw err;
@@ -38,29 +33,29 @@ exports.sendPass_Mail = async (reciever, emailSubject, emailBody, vis_name, f_pa
     try{
         // console.log(reciever, emailSubject, emailBody);
 
-        const pdfbase = fs.readFileSync(f_path, {encoding: "base64"});
 
-        const response = await mailjet.post("send", {version: "v3.1"}).request({
-            Messages:[{
-                From: {
-                    Email: "manishakesarwani9354@gmail.com",
-                    Name: "Visitor Management System"
-                },
-                To: [{
-                    Email: reciever
-                }],
-                Subject: emailSubject,
-                TextPart: "Visitor Management System Notification",
-                HTMLPart: emailBody,
-                Attachments: [{
-                    ContentType: "application/pdf",
-                    Filename: vis_name,
-                    Base64Content: pdfbase 
-                }]
+        const pdfbase = fs.readFileSync(f_path).toString("base64");
+
+        // console.log(pdfbase);
+
+        const response = await resend.emails.send({
+            from: "VMS-verified-email@visitormanagementsystem.shop",
+            to: reciever,
+            subject: emailSubject,
+            html: emailBody,
+            attachments: [{
+                name: vis_name.toLowerCase().endsWith(".pdf") ? vis_name : `${vis_name}.pdf`,
+                content: pdfbase,
+                contentType: "application/pdf",
+                filename: vis_name
             }]
-        })
-
-        console.log("Email response", response.body);
+        });
+        if(response.error){
+            console.log("Email sent failed", response.error);
+        }
+        else{
+            console.log("Email sent", response.data);
+        }
     }catch(err){
         console.log("Error", err.message);
         throw err;
